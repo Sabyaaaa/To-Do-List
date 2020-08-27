984b5e536553da7178fb777a1a7ecdb5c899b6d1
sabya <sabya7011@gmail.com>
2020-08-27 23:22:01 +0530

    Testing code on localhost

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello!");
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.write("<h1>Yay It's weekend!</h1>");
+    } else {
+        res.write("<p>It is not the weeked.</p>");
+        res.write("<h1>Boo! I have to work!</h1>");
+        res.send();
+    }
+
 });
 
 
