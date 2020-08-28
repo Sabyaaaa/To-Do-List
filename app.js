2af63c269daaed886cc3fe1966df216670d4ae9c
sabya <sabya7011@gmail.com>
2020-08-28 23:21:34 +0530

    Adding EJS templates

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// app.use("view engine", "ejs");
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
 
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
     if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Yay It's weekend!</h1>");
+        day = "Weekend";
+        res.render("List", { KindOfDay: day });
+
     } else {
-        res.write("<p>It is not the weeked.</p>");
-        res.write("<h1>Boo! I have to work!</h1>");
-        res.send();
+        day = "Weekday";
+        res.render("List", { KindOfDay: day });
     }
 
 });
-
-
 
 app.listen(3000, function() {
     console.log("Server started at port 3000");
