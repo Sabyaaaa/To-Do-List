9efbc26584b5ffed4e0de8c5ce50a0d0918411fb
sabya <sabya7011@gmail.com>
2020-08-31 22:11:40 +0530

    Desigining a stylesheet for our list

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Momos", "Eat Momos", "Buy More Momos", "Eat More Momos"];
+let items = ["Buy Momos", "Eat Momos", "Buy More Momos", "Eat More Momos"];
 
 app.set("view engine", "ejs");
 
@@ -11,22 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", { kindofDay: day, newListItem: items });
 
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
 
