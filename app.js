1532e6ee316223e3cc5ca756e281d1cc6c0f86f0
sabya <sabya7011@gmail.com>
2020-09-07 21:38:05 +0530

    Exporting day from date.js to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -11,23 +12,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    let day = date.getDay();
+    
     res.render("list", { listTitle: day, newListItems: items });
 
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     let item = req.body.newItem;
     // console.log(item);
 
@@ -41,20 +34,20 @@ app.post("/", function(req, res) {
 
 });
 
-app.get("/work", function(req, res) {
+app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/work", function(req, res) {
+app.post("/work", function (req, res) {
     let item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 });
 
-app.get("/about", function(req, res){
-    res.render("about", {listTitle: "Work List", newListItems: workItems})
+app.get("/about", function (req, res) {
+    res.render("about", { listTitle: "Work List", newListItems: workItems })
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server started at port 3000");
 });
