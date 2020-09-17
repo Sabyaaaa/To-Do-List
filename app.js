90227645c485c9813518b6b2b28b67ca1e2d671d
sabya <sabya7011@gmail.com>
2020-09-17 21:46:43 +0530

    Adding new items to the custom to do lists

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", function (req, res) {
 
     Item.find({}, function (err, foundItems) {
@@ -55,59 +62,66 @@ app.get("/", function (req, res) {
     });
 
 
-});app.get("/:customListName", function (req, res) {
+}); app.get("/:customListName", function (req, res) {
     const customListName = req.params.customListName;
-  
+
     List.findOne({ name: customListName }, function (err, foundList) {
-      if (!err) {
-        if (!foundList) {
-          // console.log("Dosen't exist!");
-  
-          //Create a new list.
-          const list = new List({
-            name: customListName,
-            items: defaultItems
-          });
-  
-          list.save();
-          res.redirect("/" + customListName);
-        } else {
-          // console.log("Exists!");
-  
-          //Show an existing list.
-          res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+        if (!err) {
+            if (!foundList) {
+                // console.log("Dosen't exist!");
+
+                //Create a new list.
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                // console.log("Exists!");
+
+                //Show an existing list.
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
         }
-      }
     });
-  
-  });
+
+});
 
 
 
 app.post("/", function (req, res) {
     const itemName = req.body.newItem;
-
+    const listName = req.body.list;
     //mongoose doc.
     const item = new Item({
         name: itemName
     });
 
-    item.save();
-
-    res.redirect("/");
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({ name: listName }, function (err, foundList) {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
 
 });
 
-app.post("/delete", function(req, res) {
-   const checkedItemId = req.body.checkbox;
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
 
-   Item.findByIdAndRemove(checkedItemId, function (err) {
-    if (!err) {
-      console.log("Removed successfully!");
-      res.redirect("/");
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (!err) {
+            console.log("Removed successfully!");
+            res.redirect("/");
 
-    }
-  });
+        }
+    });
 });
 
 app.get("/work", function (req, res) {
